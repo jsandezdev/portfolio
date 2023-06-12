73a37875e2ba8d800e01c2a6d18596f2e4f19f2a
Jordi Sández <jordisandez@gmail.com>
2023-06-12 18:14:01 +0200

    Many updates

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,9 +1,11 @@
-import { defineConfig } from 'astro/config'
-import tailwind from '@astrojs/tailwind'
-import sitemap from '@astrojs/sitemap'
+import { defineConfig } from 'astro/config';
+import tailwind from '@astrojs/tailwind';
+import sitemap from '@astrojs/sitemap';
+
+import image from "@astrojs/image";
 
 // https://astro.build/config
 export default defineConfig({
-    site: 'https://jsandez.dev',
-    integrations: [tailwind(), sitemap()]
-})
+  site: 'https://jsandez.dev',
+  integrations: [tailwind(), sitemap(), image()]
+});
